bd2423be11d9679869bcec491d6ea38eb3a086b0
zhabwe254 <zhabwe@gmail.com>
2024-06-13 17:03:33 +0300

    p

--- routes/bookingRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const bookingController = require('../controllers/bookingController');
-
-router.use('/', bookingController);
-
-module.exports = router;
--- routes/customerRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const customerController = require('../controllers/customerController');
-
-router.use('/', customerController);
-
-module.exports = router;
